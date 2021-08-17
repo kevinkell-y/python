bbe06cedd2db8c5ebf90ffd397affb6c29ebf76c
Kevin Kelly <kelly1@protonmail.com>
2021-08-17 16:40:13 -0700

    Adding directory for deliberate python practice

---
+++ tiny-python/01_hello.py
@@ -0,0 +1,13 @@
+"""
+Author  : Kevin Kelly
+Github  : @kevinkell-y
+Date    : Tuesday, August 17th, 2021
+Title   : Hello, World
+
+From Tiny Python Projects Textbook
+Chapter : 01
+Program : 01
+
+"""
+
+
